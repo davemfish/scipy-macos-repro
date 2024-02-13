96e096b54d54f53b344de43d2e33ec3f8ab18872
davemfish <davemfish@gmail.com>
2024-02-13 14:33:12 -0500

    debugging script

--- repro.py
+++ repro.py
@@ -1,2 +1,8 @@
+try:
+	from packaging.tags import sys_tags
+	_tags = list(sys_tags())
+except ImportError as err:
+	print(err)
+
 import scipy._lib._testutils
-print(0)
+print(scipy._lib._testutils.IS_MUSL)
