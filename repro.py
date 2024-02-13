3808657b96e6f100c7df93fb35f16de75d26c1c6
davemfish <davemfish@gmail.com>
2024-02-13 14:46:38 -0500

    now it works?

--- repro.py
+++ repro.py
@@ -1,10 +1,1 @@
 import scipy._lib._testutils
-print('scipy import okay')
-
-try:
-    from packaging.tags import sys_tags
-    _tags = list(sys_tags())  # raises ValueError
-except ImportError as err:
-    print(err)
-
-print(0)
